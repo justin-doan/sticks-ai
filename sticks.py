bfe57fc35c248e952b9f804b92d04b0c5746d77b
justin-doan <jmdoan8@gmail.com>
2022-06-13 14:40:19 -0500

    gui - wip

--- sticks.py
+++ sticks.py
@@ -7,6 +7,8 @@ PLAYER_1 = 1    ## Format of game board is [PLAYER_2
 PLAYER_2 = 0    ##                          PLAYER_1]
 
 BG_COLOR = (219,185,134)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
 
 # Creating a game, intiialize both players and the board
 def create_game_space():
@@ -49,8 +51,11 @@ def make_move(game, choice, turn):
     # Make move
     # Attack
     if choice == 1:
-        self_hand_choice = int(input('Which hand would you like to use, 1 for left Hand or 2 For Right Hand: ')) - 1
-        opp_hand_choice = int(input('Which of the opponents hands to attack? 1 for Left Hand or 2 For Right Hand: ')) -1
+        #self_hand_choice = int(input('Which hand would you like to use, 1 for left Hand or 2 For Right Hand: ')) - 1
+        #opp_hand_choice = int(input('Which of the opponents hands to attack? 1 for Left Hand or 2 For Right Hand: ')) -1
+
+        #TO DO implement self_hand_choice and opp_hand_choice, but instead of taking in input from command line,
+        #take in input via mouse button down, 
 
         # Cannot use dead hand to attack
         if game[turn].hands[self_hand_choice].num_fingers==0:
@@ -82,50 +87,115 @@ def make_move(game, choice, turn):
             else:
                 print("Invalid split option, choose again")
 
-# Draw board block
-# SQUARESIZE = 200
-# COLUMN_COUNT = 2
-# ROW_COUNT = 2
-# def draw_game(game):
-#     offset = SQUARESIZE
-#     for c in range(COLUMN_COUNT):
-#         for r in range(ROW_COUNT):
-#             pygame.draw.rect(screen, BG_COLOR, (c*SQUARESIZE, r*SQUARESIZE+offset, SQUARESIZE, SQUARESIZE))
 
-#     pygame.display.update()
-# Initialize Game
+# Gets the hand choice given where the mouse clicks
+def get_hand_choice(game, mouse):
+    if 0 <= mouse[0] <= 200 and 200 <= mouse[1] <= 400:
+        return 0
+    elif 200 <= mouse[0] <= 400 and 200 <= mouse[1] <= 400:
+        return 1
+    elif 0 <= mouse[0] <= 200 and 400 <= mouse[1] <= 600:
+        return 2
+    elif 200 <= mouse[0] <= 400 and 400 <= mouse[1] <= 600:
+        return 3
+    else:
+        return -1
+    
+
+def draw_game(game):
+    offset = SQUARESIZE
+
+    for c in range(COLUMN_COUNT-1):
+        for r in range(ROW_COUNT):
+            pygame.draw.rect(screen, BG_COLOR, (c*SQUARESIZE, r*SQUARESIZE+offset, SQUARESIZE, SQUARESIZE))
+
+    # Draw borders for rectangles
+    pygame.draw.line(screen, BLACK, (200,200), (200,600))
+    pygame.draw.line(screen, BLACK, (0,400), (400,400))
+
+    # Draw buttons
+    pygame.draw.rect(screen, BG_COLOR, (width * 2/3 + 25, height/2 - 75, button_width, button_height))
+    pygame.draw.rect(screen, BG_COLOR, (width * 2/3 + 25, height/2 + 75, button_width, button_height))
+
+    # Render labels for players
+    label1 = my_font.render("Player 2", 1, BG_COLOR)
+    screen.blit(label1,(25,100))
+    label2 = my_font.render("Player 1", 1, BG_COLOR)
+    screen.blit(label2, (25,625))
+
+    # Render label text for buttons
+    attack_button_label = my_font_small.render("Attack", 1, WHITE)
+    split_button_label = my_font_small.render("Split", 1, WHITE)
+    screen.blit(attack_button_label, (width * 2/3 + 37, height / 2 - 70))
+    screen.blit(split_button_label, (width * 2/3 + 45, height / 2 + 80))
+
+    #and then render all the hand values in the GUI (use images)
+    # Drawing the images
+    
+    centers = [(25,200), (225,200), (25,450), (225,450)]
+    # In range 4 => 4 hands
+    for i in range(4):
+        finger_number = game[i % 2].hands[i % 2].num_fingers
+        image = pygame.image.load(r'C:\Users\doanjust\Desktop\sticks\images\{}.png'.format(finger_number))
+        print('Image {} loaded'.format(i))
+        screen.blit(image, centers[i])
+
+    pygame.display.update()
+
 game = create_game_space() # Game is a board of 2 players
 game_over = False
 turn = PLAYER_1
 
 # Py game init block
 
-# pygame.init()
-# width = COLUMN_COUNT * SQUARESIZE
-# height = (ROW_COUNT + 2) * SQUARESIZE
-# size = (width,height)
-# screen = pygame.display.set_mode(size)
-# draw_game(game)
-# pygame.display.update()
+pygame.init()
+SQUARESIZE = 200
+COLUMN_COUNT = 3
+ROW_COUNT = 2
+
+button_width = SQUARESIZE - 50
+button_height = 50
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT + 2) * SQUARESIZE
+size = (width,height)
+screen = pygame.display.set_mode(size)
+my_font = pygame.font.SysFont("monospace",75)
+my_font_small = pygame.font.SysFont("monospace",35)
+screen.fill(WHITE)
+draw_game(game)
+
+
+
+pygame.display.update()
 
 # Game Loop
 # It will always be game[turn]'s turn
 while not game_over:
 
-    # for event in pygame.event.get():
-    #     if event.type == pygame.QUIT:
-    #         sys.exit()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
         
-    #     if event.type== pygame.MOUSEBUTTONDOWN:
-    #         print ('')
-        print_game(game)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse = pygame.mouse.get_pos()
 
-        # Prompt player for move option and take in their choice
-        print('Player 1' if turn else 'Player 2')
-        choice = int(input('Enter 1 for attack or 2 for split: '))
+            # Attack method
+            if width * 2/3 + 25 <= mouse[0] <= width * 2/3 + 175 and height / 2 - 75 <= mouse[1] <= height / 2 - 25:
+                choice = 1
 
-        make_move(game, choice, turn)
-        game_over = is_winner(game)
-        turn += 1
-        turn = turn %2
+                
+
+            if width * 2/3 + 25 <= mouse[0] <= width * 2/3 + 175 and height / 2 + 75 <= mouse[1] <= height / 2 + 150:
+                choice = 2
+
+            print_game(game)
+
+            # Prompt player for move option and take in their choice
+            print('Player 1' if turn else 'Player 2')
+
+            make_move(game, choice, turn)
+            game_over = is_winner(game)
+            turn += 1
+            turn = turn %2
     
